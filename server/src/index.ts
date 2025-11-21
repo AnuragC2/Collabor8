4b96daf3219a2af10a691aaa294622d1504b1160
Anurag Choudhary <f20201797@hyderabad.bits-pilani.ac.in>
2025-11-21 09:27:03 +0530

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,12 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/collabor8";
+const MONGO_URL = process.env.MONGO_URI || "mongodb://localhost:27017/collabor8";
 
 async function startServer() {
   try {
+
+    console.log(MONGO_URL);
     await mongoose.connect(MONGO_URL);
     console.log("Connected to MongoDB");
 
